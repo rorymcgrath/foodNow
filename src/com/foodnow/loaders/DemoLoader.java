a8f36cbef983dcc2b4cb68549b497f6c1be2f0ae
EricMcC <ericjmcclean@gmail.com>
2014-05-12 00:53:54 +0100

    Added Sample Jsoup page scrap of JustEat.ie

--- src/com/foodnow/loaders/DemoLoader.java
+++ src/com/foodnow/loaders/DemoLoader.java
@@ -4,6 +4,11 @@ import android.content.AsyncTaskLoader;
 import android.content.Context;
 import com.foodnow.models.DemoModel;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class DemoLoader extends AsyncTaskLoader<DemoModel> {
     public DemoLoader(Context context) {
         super(context);
@@ -20,8 +25,16 @@ public class DemoLoader extends AsyncTaskLoader<DemoModel> {
     @Override
     public DemoModel loadInBackground() {
         DemoModel model = new DemoModel();
-        String html = "Some Random String";
+        String html = "";
+        String url = "http://www.just-eat.ie/area/ashtown-dublin-15";
         try {
+            Document doc = Jsoup.connect(url).userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36").get();
+            //System.out.print(doc.toString());
+            Elements links = doc.select("h3");
+            for (Element link : links)
+                html += link.text() + "\n";
+
+
             Thread.sleep(5000);
         } catch(Exception e) {
 //            LOLNO
